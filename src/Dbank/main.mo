24c6f1010f38bd500285e8c0e9ef3ec10525cc95
Anurag2618g <anurag2618g@gmail.com>
2024-09-12 12:11:53 +0530

    removed the bugs and added compound function

--- src/Dbank/main.mo
+++ src/Dbank/main.mo
@@ -4,11 +4,11 @@ import Float "mo:base/Float";
 
 actor DBank {
   stable var currentValue: Float = 300;
-  currentValue := 300;
+  // currentValue := 300;
   // Debug.print(debug_show(currentValue));
 
   stable var startTime = Time.now();
-  startTime := Time.now();
+  // startTime := Time.now();
   // Debug.print(debug_show(startTime));
 
   public func topUp(amount: Float) {
--- src/Dbank_assets/src/index.js
+++ src/Dbank_assets/src/index.js
@@ -1,7 +1,12 @@
 import { Dbank } from "../../declarations/Dbank";
 
+async function update() {
+  const currentValue = await Dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentValue *100) / 100;
+}
+
 window.addEventListener("load" , async function() {
- update();
+ await update();
 });
 
 document.querySelector("form").addEventListener("submit", async function(event) {
@@ -14,24 +19,20 @@ document.querySelector("form").addEventListener("submit", async function(event) 
   
   button.setAttribute("disabled", true);
 
-  if (document.getElementById("input-amount").value.lenght != 0) {
+  if (document.getElementById("input-amount").value.length != 0) {
     await Dbank.topUp(inputAmount);
   }
 
-  if (document.getElementById("withdraw-amount").value.lenght != 0) {
+  if (document.getElementById("withdraw-amount").value.length != 0) {
     await Dbank.withdraw(outputAmount);
   }
+
   await Dbank.compound();
 
   document.getElementById("withdraw-amount").value = "";
   document.getElementById("input-amount").value = "";
   
-  update();
+  await update();
   
   button.removeAttribute("disabled");
 });
-
-async function update() {
-  const currentValue = await Dbank.checkBalance();
-  document.getElementById("value").innerText = Math.round(currentValue *100) / 100;
-}
